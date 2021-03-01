27ec074afe68ad81d1c56fed7129915fff77f348
unknown <26937163+serega-fighter@users.noreply.github.com>
2021-03-01 21:25:26 +0400

    Test data classes

--- src/main/kotlin/com/serega/basic/DataClassTest.kt
+++ src/main/kotlin/com/serega/basic/DataClassTest.kt
@@ -1,4 +1,12 @@
 package com.serega.basic
 
-class DataClassTest {
+data class DataClass(val name: String, val age: Int) {
+
+}
+
+fun main() {
+
+    println(DataClass("A", 25) == DataClass("B", 26))
+    println(DataClass("A", 25))
+    println(DataClass("A", 25).copy())
 }
