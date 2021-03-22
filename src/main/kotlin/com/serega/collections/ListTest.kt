8f824ac1d9327c4458e2555a43d78fa030a093ae
unknown <26937163+serega-fighter@users.noreply.github.com>
2021-03-22 19:45:26 +0400

    List test

--- src/main/kotlin/com/serega/collections/ListTest.kt
+++ src/main/kotlin/com/serega/collections/ListTest.kt
@@ -2,8 +2,10 @@ package com.serega.collections
 
 fun main() {
 
-    val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)
-    println(systemUsers.javaClass)
-    println(systemUsers.filter { x -> x > 5 })
-    println(systemUsers.filter { x -> x > 5 }.javaClass)
+    val testList: MutableList<Int> = mutableListOf(1, 2, 3)
+    println(testList.javaClass)
+    println(testList.filter { x -> x > 5 })
+    println(testList.filter { x -> x > 5 }.javaClass)
+
+    println(testList.any {x -> x == 2})
 }
