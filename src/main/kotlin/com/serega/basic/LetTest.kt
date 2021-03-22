b2fd21bb176626770a840a1a1d714f9041ddf9c0
unknown <26937163+serega-fighter@users.noreply.github.com>
2021-03-22 20:15:00 +0400

    Let test

--- src/main/kotlin/com/serega/basic/LetTest.kt
+++ src/main/kotlin/com/serega/basic/LetTest.kt
@@ -1,4 +1,30 @@
 package com.serega.basic
 
-class LetTest {
+fun customPrint(x: String) {
+    print("***$x***")
+}
+
+fun printNonNull(str: String?) {
+    println("Printing \"$str\":")
+
+    str?.let {                         // 4
+        print("\t")
+        customPrint(it)
+        println()
+    }
+}
+
+fun printIfBothNonNull(strOne: String?, strTwo: String?) {
+    strOne?.let { firstString ->       // 5
+        strTwo?.let { secondString ->
+            customPrint("$firstString : $secondString")
+            println()
+        }
+    }
+}
+
+fun main() {
+    printNonNull(null)
+    printNonNull("my string")
+    printIfBothNonNull("First","Second")
 }
