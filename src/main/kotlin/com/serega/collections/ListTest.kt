08f77ff32ee6e72b6df3fefde18c35c72d199436
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-05-20 12:16:01 +0400

    Advent day 2 kotlin solution

--- src/main/kotlin/com/serega/collections/ListTest.kt
+++ src/main/kotlin/com/serega/collections/ListTest.kt
@@ -2,6 +2,9 @@ package com.serega.collections
 
 fun main() {
 
+    val immutableList: List<Int> = listOf(1, 5, 5)
+    println(immutableList.javaClass)
+
     val testList: MutableList<Int> = mutableListOf(1, 2, 3)
     println(testList.javaClass)
     println(testList.filter { x -> x > 5 })
