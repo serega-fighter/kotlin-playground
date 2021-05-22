e255c4ee421bd6618f23b74daa71791e657d4320
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-05-22 12:16:01 +0400

    Types

--- src/main/kotlin/com/serega/collections/ListTest.kt
+++ src/main/kotlin/com/serega/collections/ListTest.kt
@@ -2,9 +2,11 @@ package com.serega.collections
 
 fun main() {
 
+    // class java.util.Arrays$ArrayList
     val immutableList: List<Int> = listOf(1, 5, 5)
     println(immutableList.javaClass)
 
+    // ArrayList
     val testList: MutableList<Int> = mutableListOf(1, 2, 3)
     println(testList.javaClass)
     println(testList.filter { x -> x > 5 })
