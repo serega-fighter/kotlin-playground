f3eeb57a060ee5a649f33aa9421ed0f2ddc909c6
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-06-09 12:18:05 +0400

    Extension functions test #1

--- src/main/kotlin/com/serega/basic/ExtensionFunctionsTest.kt
+++ src/main/kotlin/com/serega/basic/ExtensionFunctionsTest.kt
@@ -1,4 +1,13 @@
 package com.serega.basic
 
-class ExtensionFunctionsTest {
+fun MutableList<Int>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
+
+fun main() {
+    val list = mutableListOf(1, 2, 3)
+    list.swap(0, 2)
+    println(list)
 }
