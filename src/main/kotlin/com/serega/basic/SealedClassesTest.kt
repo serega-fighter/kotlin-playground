b61d1369436a3c39ced5561e091a3295a6b8431d
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-06-18 10:39:05 +0400

    Sealed classes

--- src/main/kotlin/com/serega/basic/SealedClassesTest.kt
+++ src/main/kotlin/com/serega/basic/SealedClassesTest.kt
@@ -1,2 +1,20 @@
 package com.serega.basic
 
+import java.io.File
+import javax.sql.DataSource
+
+sealed interface Error
+
+sealed class IOError(): Error
+
+class FileReadError(val f: File): IOError()
+class DatabaseError(val source: DataSource): IOError()
+
+fun main() {
+
+    fun log(e: Error) = when(e) {
+        is FileReadError -> { println("Error while reading file ${e.f}") }
+        is DatabaseError -> { println("Error while reading from database ${e.source}") }
+        // the `else` clause is not required because all the cases are covered
+    }
+}
