7db6d3045f7f55a2a044f10409c7a38bf36de2fe
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-12-07 09:59:01 +0400

    Duration test

--- src/main/kotlin/com/serega/duration/DurationApiTest.kt
+++ src/main/kotlin/com/serega/duration/DurationApiTest.kt
@@ -7,9 +7,11 @@ fun main() {
 
     val tenDays: Duration = 10.days
     println(tenDays)
-
+    
     val fiveDays = 5.days
     println(tenDays > fiveDays)
 
     println("${(36.hours + 30.minutes - 6.milliseconds).inWholeDays} whole days")
+
+    println(Duration.parseOrNull("11d 19h 18m"))
 }
