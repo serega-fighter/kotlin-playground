f8029df9985efc4377ab22926d99a627a6706412
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-12-12 11:31:01 +0400

    Html generation test

--- src/main/kotlin/com/serega/html/HtmlBasicTest.kt
+++ src/main/kotlin/com/serega/html/HtmlBasicTest.kt
@@ -8,6 +8,12 @@ fun main() {
         html {
             body {
                 div {
+                    b {
+                        + "AAAA"
+                    }
+                    br {
+
+                    }
                     a("https://kotlinlang.org") {
                         target = ATarget.blank + "Main site"
                     }
