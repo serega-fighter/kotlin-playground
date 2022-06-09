9f8f41630ee7c79eb998f9a1f525fc4c4cd944d6
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-06-09 10:15:45 +0300

    Test infix methods

--- src/main/kotlin/com/serega/basic/InfixFunctions.kt
+++ src/main/kotlin/com/serega/basic/InfixFunctions.kt
@@ -1,9 +1,15 @@
 package com.serega.basic
 
+infix fun Int.mult(x: Int): Int {
+    return this * x
+}
+
 fun main() {
     val pair = 1 to 2
     println(pair)
 
     val map = mapOf(1 to 2, 3 to 4)
     println(map)
+
+    println(7 mult 9)
 }
