0bf6302797e2d87c7d81e8a53657f2db9d7932d7
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-06-19 09:15:05 +0300

    Test destructiring

--- src/main/kotlin/com/serega/basic/Destructuring.kt
+++ src/main/kotlin/com/serega/basic/Destructuring.kt
@@ -11,6 +11,20 @@ class TestPair(private val field1: Int, private val field2: String) {
     }
 }
 
+class TestTriple(private val field1: Int, private val field2: String, private val field3: Double) {
+    operator fun component1(): Int {
+        return field1
+    }
+
+    operator fun component2(): String {
+        return field2
+    }
+
+    operator fun component3(): Double {
+        return field3
+    }
+}
+
 fun main() {
     val p = TestPair(5, "ABC")
     val (f1, f2) = p
@@ -23,4 +37,10 @@ fun main() {
         println(x1)
         println(x2)
     }
+
+    val tri = TestTriple(5, "A", 1.0)
+    val (y1, y2, y3) = tri
+    println(y1)
+    println(y2)
+    println(y3)
 }
