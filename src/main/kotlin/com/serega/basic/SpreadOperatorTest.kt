9c5656e94f494fe055d78f882771bda3ab336d07
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-06-22 11:36:30 +0300

    Test spread operator

---
+++ src/main/kotlin/com/serega/basic/SpreadOperatorTest.kt
@@ -0,0 +1,17 @@
+package com.serega.basic
+
+fun <T> printAll(vararg ts: T) {
+    ts.forEach { println(it) }
+}
+
+// Kotlin will use the Arrays.copyOf(array, length) method to first create a copy of the spreading array.
+// Then it will pass the new array as the vararg parameter:
+
+fun main() {
+    val arr = arrayOf("ABC", "CDE")
+
+    printAll(arr)
+
+    printAll(*arr)
+
+}
