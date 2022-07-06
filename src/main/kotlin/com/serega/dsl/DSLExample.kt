97f07be86ced8006304878d50dc7774cafe69895
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-07-06 19:15:03 +0300

    Test custom DSL

--- src/main/kotlin/com/serega/dsl/DSLExample.kt
+++ src/main/kotlin/com/serega/dsl/DSLExample.kt
@@ -1,4 +1,46 @@
 package com.serega.dsl
 
-class DSLExample {
+interface Element {
+
+}
+
+class HTML {
+
+    private val children: MutableList<Element> = mutableListOf()
+
+    fun head(init: Head.() -> Unit) = initTag(Head(), init)
+
+    fun body(init: Body.() -> Unit) = initTag(Body(), init)
+
+    protected fun <T : Element> initTag(tag: T, init: T.() -> Unit): T {
+        tag.init()
+        children.add(tag)
+        return tag
+    }
+}
+
+class Head : Element {
+
+}
+
+class Body : Element {
+
+}
+
+fun html(initMethod: HTML.() -> Unit): HTML {
+    val html = HTML()
+    html.initMethod()
+    return html
+}
+
+fun main() {
+
+    html {
+        head {
+
+        }
+        body {
+
+        }
+    }
 }
