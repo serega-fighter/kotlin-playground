c8141e6dd5f6a7c10e27739cb703d9050eaa7581
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-10-20 11:30:05 +0300

    operator overload

---
+++ src/main/kotlin/com/serega/collections/OperatorOveraloadTest.kt
@@ -0,0 +1,19 @@
+package com.serega.collections
+
+class Test(val value: Int) {
+
+    operator fun plus(other: Test): Test {
+        return Test(this.value + other.value)
+    }
+
+    override fun toString(): String {
+        return "Test(value=$value)"
+    }
+
+    
+}
+
+fun main() {
+
+    println(Test(5) + Test(6))
+}
