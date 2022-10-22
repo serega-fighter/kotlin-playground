866cb60de9ebb7b600f988af940abde1b7cb4a88
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-10-22 11:09:05 +0300

    rename data class

---
+++ src/main/kotlin/com/serega/lang/basic/Person.kt
@@ -0,0 +1,16 @@
+package com.serega.lang.basic
+
+data class Person(val name: String, val age: Int) {
+
+}
+
+fun print(p: Person) {
+    println(p)
+}
+
+fun main() {
+
+    println(Person("A", 25) == Person("B", 26))
+    println(Person("A", 25).copy())
+    print(Person("ABC", 40))
+}
