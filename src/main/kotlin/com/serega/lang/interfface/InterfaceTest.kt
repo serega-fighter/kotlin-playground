50869779dfda2d95d4eb213ae312a446b5c49de3
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-10-27 12:02:05 +0300

    interface with prop

--- src/main/kotlin/com/serega/lang/interfface/InterfaceTest.kt
+++ src/main/kotlin/com/serega/lang/interfface/InterfaceTest.kt
@@ -1,2 +1,13 @@
 package com.serega.lang.interfface
 
+interface Element {
+    val active: Boolean
+}
+
+class ActualElement: Element {
+    override var active: Boolean = false
+}
+
+fun main() {
+
+}
