9d08a45ddcb0e873ef99d4dfc09e11bfb456d65f
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-11-10 11:45:05 +0300

    require block

---
+++ src/main/kotlin/com/serega/lang/basic/require_check_blocks.kt
@@ -0,0 +1,15 @@
+package com.serega.lang.basic
+
+fun factorial(n: Int): Int {
+    require(n >= 0)
+    return 5
+}
+
+fun safeToString(obj: Any?) {
+    requireNotNull(obj)
+    println(obj)
+}
+
+fun main() {
+    
+}
