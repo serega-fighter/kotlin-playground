93b348cd5cac2e87738c1d51d497edf88f30fbb8
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-11-12 12:15:43 +0300

    check state

---
+++ src/main/kotlin/com/serega/lang/err/prefer_failure_or_null_when_err_expected.kt
@@ -0,0 +1,27 @@
+package com.serega.lang.err
+
+/*
+This is why the rule is that we should prefer
+returning null or Failure when an error is expected, and
+throwing an exception when an error is not expected
+*/
+
+inline fun <reified T> String.readObject(default: T): Result<T> {
+    //...
+    var incorrectSign = false // ...
+    if(incorrectSign) {
+        return Failure(JsonParsingException())
+    }
+    //...
+    return Success(default)
+}
+
+sealed class Result<out T>
+class Success<out T>(val result: T): Result<T>()
+class Failure(val throwable: Throwable): Result<Nothing>()
+
+class JsonParsingException: Exception()
+
+fun main() {
+
+}
