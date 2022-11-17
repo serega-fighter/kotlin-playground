2ff7b0fd7383b45054ae54be5be0429ea97e1a76
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-11-17 11:39:05 +0300

    use block example

---
+++ src/main/kotlin/com/serega/lang/basic/use_block_test.kt
@@ -0,0 +1,18 @@
+package com.serega.lang.basic
+
+import java.io.BufferedReader
+import java.io.Closeable
+import java.io.FileReader
+
+public inline fun <T : Closeable?, R> T.use2(block: (T) -> R): R {
+    return block(this)
+}
+
+fun main() {
+
+    val reader = BufferedReader(FileReader("data/advent/day1.txt"))
+    val s: String? = reader.use2 {
+        it.readLine()
+    }
+    println(s)
+}
