5553083ffc6551b125c1a7ab32b428419d7f510b
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-12-07 09:41:05 +0300

    del

--- src/main/kotlin/com/serega/lang/collections/list_test.kt
+++ src/main/kotlin/com/serega/lang/collections/list_test.kt
@@ -1,21 +1,7 @@
 package com.serega.lang.collections
 
 fun main() {
-
-    // class java.util.Arrays$ArrayList
-    val immutableList: List<Int> = listOf(1, 5, 5)
-    println(immutableList.javaClass)
-
-    // ArrayList
     val mutableList: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
-    println(mutableList.javaClass)
-    println(mutableList.filter { x -> x > 5 })
-    println(mutableList.filter { x -> x > 5 }.javaClass)
-
-    println(mutableList.any { x -> x == 2})
-
-    mutableList.removeIf { it == 2 }
-    println(mutableList)
 
     for ((index, value) in mutableList.withIndex()) {
         println("the element at $index is $value")
