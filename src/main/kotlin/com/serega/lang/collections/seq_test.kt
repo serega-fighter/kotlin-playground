7e36cbbed43cf3979078942eef1c23d9a0c807fd
Serega <26937163+serega-fighter@users.noreply.github.com>
2023-08-03 23:43:12 +0300

    add seq test

--- src/main/kotlin/com/serega/lang/collections/seq_test.kt
+++ src/main/kotlin/com/serega/lang/collections/seq_test.kt
@@ -1,4 +1,26 @@
 package com.serega.lang.collections
 
-class seq_test {
+fun sumOfPrimes(limit: Int): Long {
+    val seq: Sequence<Long> = sequenceOf(2L) +
+            generateSequence(3L, {
+                it + 2
+            }).takeWhile{
+                it < limit
+            }
+    fun isPrime(n: Long): Boolean =
+        seq.takeWhile {
+            it * it <= n
+        }.all {
+            n % it != 0L
+        }
+    return seq.filter(::isPrime).sum()
+}
+
+fun main() {
+    val generateSequence = generateSequence(5) {
+        it + 2
+    }
+    println(generateSequence.takeWhile { it < 20 }.any())
+
+    println(sumOfPrimes(5))
 }
