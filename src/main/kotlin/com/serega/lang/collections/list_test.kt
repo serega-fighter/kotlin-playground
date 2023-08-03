1fc4b1ae7ff331c7fc78cf43ea4a711a5c8dcbc6
Serega <26937163+serega-fighter@users.noreply.github.com>
2023-08-03 23:43:12 +0300

    add seq test

--- src/main/kotlin/com/serega/lang/collections/list_test.kt
+++
@@ -1,9 +0,0 @@
-package com.serega.lang.collections
-
-fun main() {
-    val mutableList: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
-
-    for ((index, value) in mutableList.withIndex()) {
-        println("the element at $index is $value")
-    }
-}
---
+++ src/main/kotlin/com/serega/lang/collections/seq_test.kt
@@ -0,0 +1,4 @@
+package com.serega.lang.collections
+
+class seq_test {
+}
